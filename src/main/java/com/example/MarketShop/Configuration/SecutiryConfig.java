6f19ef1351b6565d3f3e20b15b9d04e4bcbd5e32
LienHung <kozaxlh@gmail.com>
2023-06-22 02:47:10 +0700

    authorization by OAuth2

--- src/main/java/com/example/MarketShop/Configuration/SecutiryConfig.java
+++ src/main/java/com/example/MarketShop/Configuration/SecutiryConfig.java
@@ -1,21 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/springframework/Configuration.java to edit this template
- */
 package com.example.MarketShop.Configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-/**
- *
- * @author Admin
- */
 @Configuration
-public class SecurityConfig {
+public class SecutiryConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
@@ -25,10 +16,5 @@ public class SecurityConfig {
 //                .and()
 //                .oauth2Login();
         return http.build();
-    }
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder(){
-        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-        return bCryptPasswordEncoder;
     }
 }
