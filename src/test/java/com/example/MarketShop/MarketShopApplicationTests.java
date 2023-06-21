e7d0f3a6fb01dd5acad54a4e1533b1461953061c
LienHung <kozaxlh@gmail.com>
2023-06-22 03:24:17 +0700

    fix github action

--- src/test/java/com/example/MarketShop/MarketShopApplicationTests.java
+++ src/test/java/com/example/MarketShop/MarketShopApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.MarketShop;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = MarketShopApplicationTests.class)
 class MarketShopApplicationTests {
 
 	@Test
