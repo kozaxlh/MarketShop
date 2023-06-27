ef810d0fcb06d2a0bd00cebba62292ffac5b0f7d
LienHung <kozaxlh@gmail.com>
2023-06-27 18:39:51 +0700

    update Users Repo

--- src/main/java/com/example/MarketShop/Repository/UsersRepository.java
+++ src/main/java/com/example/MarketShop/Repository/UsersRepository.java
@@ -3,5 +3,8 @@ package com.example.MarketShop.Repository;
 import com.example.MarketShop.Model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsersRepository extends JpaRepository<Users, Integer> {
+    Optional<Users> findByEmail(String email);
 }
