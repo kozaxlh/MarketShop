196cd25b4ca1b85831ef1412e4a9fd2657469b0f
LienHung <kozaxlh@gmail.com>
2023-06-29 03:01:57 +0700

    handle register

--- src/main/java/com/example/MarketShop/Repository/RolesRepository.java
+++ src/main/java/com/example/MarketShop/Repository/RolesRepository.java
@@ -4,8 +4,6 @@ import com.example.MarketShop.Model.Roles;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-import java.util.Collection;
-
 public interface RolesRepository extends JpaRepository<Roles, Integer>, JpaSpecificationExecutor<Roles> {
-
+    Roles findByRoleName(String roleName);
 }
