0984da4754d79f801f8dbc7d52510db855c78906
LienHung <kozaxlh@gmail.com>
2023-07-18 19:19:52 +0700

    add orders handle

--- src/main/java/com/example/MarketShop/Service/Implement/OrdersServiceImpl.java
+++ src/main/java/com/example/MarketShop/Service/Implement/OrdersServiceImpl.java
@@ -1,17 +1,44 @@
 package com.example.MarketShop.Service.Implement;
 
 import com.example.MarketShop.DTO.OrdersDTO;
+import com.example.MarketShop.Exception.AppException;
 import com.example.MarketShop.Model.Orders;
+import com.example.MarketShop.Model.Users;
 import com.example.MarketShop.Repository.OrdersRepository;
+import com.example.MarketShop.Repository.UsersRepository;
+import com.example.MarketShop.Service.Interface.OrderdetailService;
 import com.example.MarketShop.Service.Interface.OrdersService;
-import org.springframework.beans.BeanUtils;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-
-import java.util.NoSuchElementException;
 
 @Service
 public class OrdersServiceImpl implements OrdersService {
+    private OrdersRepository ordersRepository;
+    private OrderdetailService orderdetailService;
+    private UsersRepository usersRepository;
+    private ModelMapper modelMapper;
 
+    @Autowired
+    public OrdersServiceImpl(OrdersRepository ordersRepository, OrderdetailService orderdetailService,
+                             UsersRepository usersRepository, ModelMapper modelMapper) {
+        this.ordersRepository = ordersRepository;
+        this.orderdetailService = orderdetailService;
+        this.usersRepository = usersRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public OrdersDTO addOrder(OrdersDTO ordersDTO, String sessionEmail) {
+        Users user = usersRepository.findByEmail(sessionEmail).get();
+
+        Orders orders = modelMapper.map(ordersDTO, Orders.class);
+
+        if (orders.getProductList().isEmpty()) {
+            throw new AppException(HttpStatus.BAD_REQUEST, "Không có sản phẩm trong giỏ hàng");
+        }
+
+        return modelMapper.map(ordersRepository.save(orders), OrdersDTO.class);
+    }
 }
--- src/main/java/com/example/MarketShop/Service/Interface/OrdersService.java
+++ src/main/java/com/example/MarketShop/Service/Interface/OrdersService.java
@@ -1,4 +1,7 @@
 package com.example.MarketShop.Service.Interface;
 
+import com.example.MarketShop.DTO.OrdersDTO;
+
 public interface OrdersService {
+    OrdersDTO addOrder(OrdersDTO ordersDTO, String sessionEmail);
 }
