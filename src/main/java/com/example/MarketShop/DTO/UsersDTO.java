ae86431d54b4971e6eccf4d5d97b5e09ad346020
LienHung <kozaxlh@gmail.com>
2023-08-08 23:58:11 +0700

    add more att for DTO

--- src/main/java/com/example/MarketShop/DTO/UsersDTO.java
+++ src/main/java/com/example/MarketShop/DTO/UsersDTO.java
@@ -1,10 +1,12 @@
 package com.example.MarketShop.DTO;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonFilter;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonView;
 import jakarta.validation.constraints.Email;
@@ -15,7 +17,6 @@ import lombok.NoArgsConstructor;
 import net.minidev.json.annotate.JsonIgnore;
 
 /**
- *
  * @author Admin
  */
 @Data
@@ -24,7 +25,7 @@ import net.minidev.json.annotate.JsonIgnore;
 @JsonFilter("UsersInfo")
 public class UsersDTO implements Serializable {
     private Integer userID;
-    
+
     @NotEmpty(message = "Email should not be empty")
     @Email
     private String email;
@@ -36,6 +37,12 @@ public class UsersDTO implements Serializable {
     private String address;
 
     private String city;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime create_at;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime update_at;
 
     private Set usersRole;
 
