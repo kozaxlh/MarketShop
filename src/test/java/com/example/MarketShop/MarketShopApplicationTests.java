b7a984c95122ec6b3586c184089af8574614bbd3
LienHung <kozaxlh@gmail.com>
2023-09-13 01:49:35 +0700

    run jar

--- src/test/java/com/example/MarketShop/MarketShopApplicationTests.java
+++ src/test/java/com/example/MarketShop/MarketShopApplicationTests.java
@@ -1,13 +1,7 @@
 package com.example.MarketShop;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes = MarketShopApplicationTests.class)
 class MarketShopApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
